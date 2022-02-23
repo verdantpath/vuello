55b23616847d4c198063ea18924425ecebaf030b
verdantpath <m_dittmann@yahoo.com>
2022-02-22 21:46:00 -0800

    Add top bar and nav drawer, sign out functionality

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
+  router: {
+    middleware: ['auth']
+  },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
