481fc8925d8dc284c48339230bda12fbb861868a
verdantpath <m_dittmann@yahoo.com>
2022-02-28 21:12:20 -0800

    Add date filter, board view

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,9 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    '@/plugins/filters'
+  ],
   router: {
     middleware: ['auth']
   },
---
+++ pages/boards/_id.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>inside our board</h1>
+</template>
+<script>
+  export default {
+    
+  }
+</script>
+<style>
+
+</style>
