e9ce48696f4ba9137ca04ba15aab887d5e14098c
verdantpath <m_dittmann@yahoo.com>
2022-03-02 22:53:56 -0800

    Edit and update boards and cards

--- pages/boards/_id.vue
+++ pages/boards/_id.vue
@@ -135,11 +135,189 @@
     </div>
   </div>
 </template>
+
 <script>
+  import { v4 as uuidv4 } from 'uuid'
   export default {
-    
+    layout: 'board',
+    data() {
+      return {
+        listId: '',
+        list: {
+          title: '',
+        },
+        card: {
+          title:'',
+        },
+        currentCard: {},
+        cardDraggedId: '',
+        cardDraggedListId: '',
+        dialog: false,
+        dialogCard: false,
+        dialogEditCard: false,
+        drawer: false,
+      }
+    },
+    async asyncData({ params }) {
+      // lets get our board data before page load, and then after that await changes
+      let boardRef = $nuxt.$fire.firestore
+        .collection('users')
+        .doc($nuxt.$fire.auth.currentUser.uid)
+        .collection('boards')
+        .doc(params.id)
+      let boardData = {}
+      await boardRef
+        .get()
+        .then(function(doc) {
+          if (doc.exists) {
+            boardData = doc.data()
+            boardData.id = params.id
+          }
+        })
+        .catch(function (error) {})
+      // if (boardData.color != '' || boardData.image.downloadURL ! = '') {
+      //    $nuxt.$emit('togle-alt-topbar')
+      //  }
+      return { board: boardData }
+    },
+    created() {
+      let that = this
+      let tempId = this.board.id
+      let boardRef = $nuxt.$fire.firestore
+        .collection('users')
+        .doc($nuxt.$fire.auth.currentUser.uid)
+        .collection('boards')
+        .doc(tempId)
+        .onSnapshot((doc) => {
+          if (doc.exists) {
+            that.board = doc.data()
+            that.board.id = tempId
+          } 
+        })
+    },
+    methods: {
+      async createList() {
+        let that = this
+        that.dialog = false
+        if (that.list.title != '') {
+          // add to firebase
+          // lets give our list a created date
+          that.list.id = uuidv4()
+          that.list.cards = []
+          that.list.dateCreated = Date.now()
+          if (that.board.lists) {
+            that.board.lists.push(that.list)
+          } else {
+            that.board.lists = []
+            that.board.lists.push(that.list)
+          }
+          await that.updateBoard()
+          that.list = {}
+        }
+      },
+      async updateCardList(newlistId) {
+        let that = this
+        let tempListIndex = -1
+        let tempCardIndex = -1
+        let newListIndex = -1
+        let tempListCount = 0
+        let tempCardCount = 0
+
+        // get the index in current cards current list
+        for (const list of that.board.lists) {
+          if (list.id === newlistId) {
+            newListIndex = tempListCount
+          }
+          if (that.currentCard.listId === list) {
+            // correct list, now find card
+            tempListIndex = tempListCount
+            for (const card of list.cards) {
+              if (card.id === that.currentCard.id) {
+                tempCardIndex = tempCardCount
+              }
+              tempCardCount++
+            }
+          }
+          tempListCount++
+        }
+
+        // remove currentCard from current list
+        that.board.lists[tempListIndex].cards.splice(tempCardIndex, 1)
+
+        // add currentCard to its new list
+        that.currentCard.listId = newlistId
+        that.board.lists[newListIndex].cards.push(that.currentCard)
+
+        await that.updateBoard()
+      },
+      allowDrop(ev) {
+        ev.preventDefault()
+      },
+      drag(ev, card) {
+        this.currentCard = card
+      },
+      drop(ev, listId) {
+        ev.preventDefault()
+        this.updateCardList(listId)
+      },
+      async deleteList(listId) {
+        let that = this
+        let index = -1
+        let count = 0
+        for (const list of that.board.lists) {
+          if (list.id == listId) {
+            index - count
+          }
+          count++
+        }
+        if (index > -1) {
+          that.board.lists.splice(index, 1)
+          await that.updateBoard()
+        }
+      },
+      async createCard() {
+        let that = this
+        that.dialogCard = false
+        // show modal to capture card name
+        // add card
+        if (that.card.title != '') {
+          // add to firebase
+          // lets give our card a created date
+          that.card.id = uuidv4()
+          that.card.dateCreated = Date.now()
+          that.card.listId = that.listId
+          if (that.board.lists) {
+            let index = -1
+            let count = 0
+            for (const list of that.board.lists) {
+              if (list.id === that.listId) {
+                index = count
+              }
+              count++
+            }
+            if (index != -1) {
+              // we found the list, now push our card
+              if (that.board.lists[index].cards) {
+                that.board.lists[index].cards.pupsh(that.card)
+              } else {
+                that.board.lists[index].cards = []
+                that.board.lists[index].cards.push(that.card)
+              }
+            }
+          }
+          await that.updateBoard()
+          that.card = {}
+          that.listId = ''
+        }
+      },
+      editCard(card) {
+        this.dialogEditCard = true
+        this.currentCard = card
+      },
+    },
   }
 </script>
+
 <style>
 
 </style>
